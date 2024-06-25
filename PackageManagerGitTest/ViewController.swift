d2faa5755e5cd84625de7c19cf14351b96257908
Kim-Dong-Hyeon <dhkim960206@gmail.com>
2024-06-25 18:27:42 +0900

    first commit

---
+++ PackageManagerGitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PackageManagerGitTest
+//
+//  Created by 김동현 on 6/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+<H1>Xcode Swift Package Manager .gitignore TEST</H1>
